6fb885fcc31ce8f982a4c53fff6051218b190dea
e-v-antonov <antonov.eugeny2012@yandex.ru>
2019-06-02 20:31:10 +0300

    Формирование документа Регистрационная карточка сделано по правилам и по примеру журнала, НО не выводит текст как надо

--- Library/Library/RegistrationCardWord.cs
+++ Library/Library/RegistrationCardWord.cs
@@ -24,103 +24,78 @@ namespace Library
 
             object missing = Type.Missing;
             word.Document document = application.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+            //word.Range range = document.Range(0, 0);
+            //range.ParagraphFormat.Alignment = word.WdParagraphAlignment.wdAlignParagraphCenter;
 
             string fileName = @"D:\Games\Пример.docx";
 
-                document.Sections.PageSetup.LeftMargin = application.CentimetersToPoints(2);
-                document.Sections.PageSetup.RightMargin = application.CentimetersToPoints(1);
-                document.Sections.PageSetup.TopMargin = application.CentimetersToPoints(2);
-                document.Sections.PageSetup.BottomMargin = application.CentimetersToPoints(2);
-                document.Sections.PageSetup.VerticalAlignment = word.WdVerticalAlignment.wdAlignVerticalTop;
+            document.Sections.PageSetup.LeftMargin = application.CentimetersToPoints(2);
+            document.Sections.PageSetup.RightMargin = application.CentimetersToPoints(1);
+            document.Sections.PageSetup.TopMargin = application.CentimetersToPoints(2);
+            document.Sections.PageSetup.BottomMargin = application.CentimetersToPoints(2);                     
 
-            word.Paragraph para = document.Paragraphs.Add(ref missing);
-           
+            word.Paragraph title = document.Paragraphs.Add();
+            title.Format.Alignment = word.WdParagraphAlignment.wdAlignParagraphCenter;
+            title.Range.Font.Name = "Times New Roman";
+            title.Range.Font.Size = 16;
+            title.Range.Bold = 2;
+            title.Range.ParagraphFormat.SpaceAfter = 0;
+            title.Range.ParagraphFormat.SpaceBefore = 0;
+            title.Range.ParagraphFormat.LineSpacingRule = word.WdLineSpacing.wdLineSpace1pt5;
+            title.Range.Text = "Регистрационная карточка читателя\nбиблиотеки №127";
+
+            //word.Range range = document.Range(0, 40);
+            //range.ParagraphFormat.Alignment = word.WdParagraphAlignment.wdAlignParagraphCenter;                  
+
+            word.Paragraph text = document.Paragraphs.Add();
+            text.Format.Alignment = word.WdParagraphAlignment.wdAlignParagraphLeft;
+            text.Range.Font.Name = "Times New Roman";
+            text.Range.Font.Size = 14;
+            text.Range.Bold = 0;
+            text.Range.ParagraphFormat.SpaceAfter = 0;
+            text.Range.ParagraphFormat.SpaceBefore = 0;
+            text.Range.ParagraphFormat.LineSpacingRule = word.WdLineSpacing.wdLineSpace1pt5;
+            text.Range.Text = "Регистрационный номер:\nДата заполнения:\nФамилия:\nИмя:\nОтчество:\nДата рождения:\nСерия и номер паспорта:\nКем выдан:\nКогда выдан:\nАдрес проживания:\nДомашний телефон:\nМобильный телефон:\nАдрес электронной почты:\n";
+
+            document.Paragraphs.Add();
+
+            word.Paragraph agreement = document.Paragraphs.Add();
+            agreement.Format.Alignment = word.WdParagraphAlignment.wdAlignParagraphJustify;
+            agreement.Range.Font.Name = "Times New Roman";
+            agreement.Range.Font.Size = 14;
+            agreement.Range.ParagraphFormat.SpaceAfter = 0;
+            agreement.Range.ParagraphFormat.SpaceBefore = 0;
+            agreement.Range.ParagraphFormat.LineSpacingRule = word.WdLineSpacing.wdLineSpace1pt5;
+            //agreement.Range.ParagraphFormat.LeftIndent = application.CentimetersToPoints(Convert.ToSingle(1.5));
+            agreement.Range.Text = "    Подтверждаю, что я ознакомлен и полностью согласен с условиями оказания мне библиотечных услуг библиотекой №127 изложенными в «Правилах пользования библиотекой №127», я согласен с тем, что библиотека может отказать мне в обслуживании в случае их нарушения. Также даю свое согласие на обработку моих персональных данных, указанных в настоящей регистрационной карточке.";
+
+            word.Paragraph agreement1 = document.Paragraphs.Add();
+            agreement1.Format.Alignment = word.WdParagraphAlignment.wdAlignParagraphJustify;
+            agreement1.Range.Font.Name = "Times New Roman";
+            agreement1.Range.Font.Size = 14;
+            agreement1.Range.ParagraphFormat.SpaceAfter = 0;
+            agreement1.Range.ParagraphFormat.SpaceBefore = 0;
+            agreement1.Range.ParagraphFormat.LineSpacingRule = word.WdLineSpacing.wdLineSpace1pt5;
+            agreement1.Range.Text = "    Данное согласие действует до моего прямого отказа от пользования услугами библиотеки, выраженного мною лично в устной или письменной форме.";
+
+            word.Paragraph signature = document.Paragraphs.Add();
+            signature.Format.Alignment = word.WdParagraphAlignment.wdAlignParagraphRight;
+            signature.Range.Font.Name = "Times New Roman";
+            signature.Range.Font.Size = 14;
+            signature.Range.ParagraphFormat.SpaceAfter = 0;
+            signature.Range.ParagraphFormat.SpaceBefore = 0;
+            signature.Range.ParagraphFormat.LineSpacingRule = word.WdLineSpacing.wdLineSpaceSingle;
+            signature.Range.Text = "____________ / ____________";
             
-            //para.Range.ParagraphFormat.Alignment = word.WdParagraphAlignment.wdAlignParagraphCenter;
-            para.Range.Font.Name = "Times New Roman";
-            para.Range.Bold = 2;
-            para.Range.Font.Size = 16;
-            para.Range.ParagraphFormat.SpaceAfter = 0;
-            para.Range.ParagraphFormat.SpaceBefore = 0;
-            para.Range.ParagraphFormat.LineSpacingRule = word.WdLineSpacing.wdLineSpace1pt5;
-            para.Range.Text = "Регистрационная карточка читателя";
-          
-            para.Range.InsertParagraphAfter();
-            para.Range.Text = "библиотеки №127";
-
-            para.Range.InsertParagraphAfter();
-            //para.Range.ParagraphFormat.Alignment = word.WdParagraphAlignment.wdAlignParagraphLeft;
-            para.Range.Font.Name = "Times New Roman";
-            para.Range.Font.Size = 14;
-            para.Range.ParagraphFormat.SpaceAfter = 0;
-            para.Range.ParagraphFormat.SpaceBefore = 0;
-            para.Range.ParagraphFormat.LineSpacingRule = word.WdLineSpacing.wdLineSpace1pt5;
-            para.Range.Text = "Регистрационный номер:\nДата заполнения:\nФамилия:\nИмя:\nОтчество:\nДата рождения:\nСерия и номер паспорта:\nКем выдан:\nКогда выдан:\nАдрес проживания:\nДомашний телефон:\nМобильный телефон:\nАдрес электронной почты:\n";
-            
-
-            //object oMissing = System.Reflection.Missing.Value;
-            //document.Paragraphs.Add(ref oMissing);
-            //document.Paragraphs.Add(ref oMissing);
-            //document.Paragraphs.Add(ref oMissing);
-            //document.Paragraphs.Add(ref oMissing);
-            //document.Paragraphs.Add(ref oMissing);
-            //document.Paragraphs.Add(ref oMissing);
-            //document.Paragraphs.Add(ref oMissing);
-
-
-
-            //listParagraph = document.Paragraphs;
-
-            //wordparagraph = (word.Paragraph)listParagraph[1];
-            //wordparagraph.Range.Text = "Регистрационная карточка читателя";
-            //wordparagraph.Range.ParagraphFormat.Alignment = word.WdParagraphAlignment.wdAlignParagraphCenter;
-            //wordparagraph.Range.Font.Name = "Times New Roman";
-            //wordparagraph.Range.Bold = 2;
-            //wordparagraph.Range.Font.Size = 16;
-            //wordparagraph.Range.ParagraphFormat.SpaceAfter = 0;
-            //wordparagraph.Range.ParagraphFormat.SpaceBefore = 0;
-            //wordparagraph.Range.ParagraphFormat.LineSpacingRule = word.WdLineSpacing.wdLineSpace1pt5;
-
-            //wordparagraph = (word.Paragraph)listParagraph[2];
-            //wordparagraph.Range.Text = "библиотеки №127";
-            //wordparagraph.Range.ParagraphFormat.Alignment = word.WdParagraphAlignment.wdAlignParagraphCenter;
-            //wordparagraph.Range.Font.Name = "Times New Roman";
-            //wordparagraph.Range.Bold = 2;
-            //wordparagraph.Range.Font.Size = 16;
-            //wordparagraph.Range.ParagraphFormat.SpaceAfter = 0;
-            //wordparagraph.Range.ParagraphFormat.SpaceBefore = 0;
-            //wordparagraph.Range.ParagraphFormat.LineSpacingRule = word.WdLineSpacing.wdLineSpace1pt5;
-
-            //wordparagraph = (word.Paragraph)listParagraph[3];
-            //wordparagraph.Range.Text = "Регистрационный номер:\nДата заполнения:\nФамилия:\nИмя:\nОтчество:\nДата рождения:\nСерия и номер паспорта:\nКем выдан:\nКогда выдан:\nАдрес проживания:\nДомашний телефон:\nМобильный телефон:\nАдрес электронной почты:\n";
-            //wordparagraph.Range.ParagraphFormat.Alignment = word.WdParagraphAlignment.wdAlignParagraphLeft;
-            //wordparagraph.Range.Font.Name = "Times New Roman";
-            //wordparagraph.Range.Font.Size = 14;
-            //wordparagraph.Range.ParagraphFormat.SpaceAfter = 0;
-            //wordparagraph.Range.ParagraphFormat.SpaceBefore = 0;
-            //wordparagraph.Range.ParagraphFormat.LineSpacingRule = word.WdLineSpacing.wdLineSpace1pt5;
-
-            //wordparagraph = (word.Paragraph)listParagraph[4];
-            //wordparagraph.Range.Text = "";
-            //wordparagraph.Range.ParagraphFormat.Alignment = word.WdParagraphAlignment.wdAlignParagraphLeft;
-            //wordparagraph.Range.Font.Name = "Times New Roman";
-            //wordparagraph.Range.Font.Size = 14;
-            //wordparagraph.Range.ParagraphFormat.SpaceAfter = 0;
-            //wordparagraph.Range.ParagraphFormat.SpaceBefore = 0;
-            //wordparagraph.Range.ParagraphFormat.LineSpacingRule = word.WdLineSpacing.wdLineSpace1pt5;
-
-            //wordparagraph = (word.Paragraph)listParagraph[16];
-            //wordparagraph.Range.Text = "Подтверждаю, что я ознакомлен и полностью согласен с условиями оказания мне библиотечных услуг библиотекой №127 изложенными в «Правилах пользования библиотекой №127», я согласен с тем, что библиотека может отказать мне в обслуживании в случае их нарушения. Также даю свое согласие на обработку моих персональных данных, указанных в настоящей регистрационной карточке.";
-            //wordparagraph.Range.ParagraphFormat.Alignment = word.WdParagraphAlignment.wdAlignParagraphJustify;
-            //wordparagraph.Range.Font.Name = "Times New Roman";
-            //wordparagraph.Range.Font.Size = 14;
-            //wordparagraph.Range.ParagraphFormat.SpaceAfter = 0;
-            //wordparagraph.Range.ParagraphFormat.SpaceBefore = 0;
-            //wordparagraph.Range.ParagraphFormat.LineSpacingRule = word.WdLineSpacing.wdLineSpace1pt5;
-
-            //document.SaveAs2(fileName, word.WdSaveFormat.wdFormatDocumentDefault);
-            //document.Close();
-            //application.Quit();
+            word.Paragraph decryptione = document.Paragraphs.Add();
+            decryptione.Format.Alignment = word.WdParagraphAlignment.wdAlignParagraphLeft;
+            decryptione.Range.Font.Name = "Times New Roman";
+            decryptione.Range.Font.Size = 10;
+            decryptione.Range.Font.Bold = 2;
+            decryptione.Range.ParagraphFormat.SpaceAfter = 0;
+            decryptione.Range.ParagraphFormat.SpaceBefore = 0;
+            decryptione.Range.ParagraphFormat.LineSpacingRule = word.WdLineSpacing.wdLineSpaceSingle;
+            decryptione.Range.Text = "										Подпись                 Расшифровка";
 
             document.SaveAs2(fileName, word.WdSaveFormat.wdFormatDocumentDefault, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing);
             object save_changes = false;
